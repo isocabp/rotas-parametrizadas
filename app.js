12b6f9174f584cf2aca6863d2e1f376ee7dea695
Isabella Borges <isabellab.ferreira5@gmail.com>
2022-05-19 12:09:03 -0300

    feat:rodando servidor com rotas parametrizadas

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+let app = express();
+
+app.get('/', (req, res)=>res.send("olá, mundo!"));
+app.get('/contatos', (req, res)=>res.send("Página contatos"));
+app.get('/produtos/:id?', (req,res)=>{
+    let {id} = req.params;
+    res.send("Eu tenho um produto com o id: " + id)
+})
+//criando um servidor
+app.listen(3000, ()=>console.log("Servidor rodando na porta 3000"))
